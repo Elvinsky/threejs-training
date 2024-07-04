4d8ef820f743612c4768fccbd00ae755d89eca08
Nikita <mikhnevichnik@gmail.com>
2024-07-04 16:13:02 +0200

    feat: added main movemnt logic and sprite animation

---
+++ src/objects/player.ts
@@ -0,0 +1,69 @@
+import {
+  FrontSide,
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  Quaternion,
+  RepeatWrapping,
+  Texture,
+  TextureLoader,
+  Vector3,
+} from 'three';
+import { createArrowHelper } from './arrowHelper';
+
+const idle = (texture: Texture, time: number) => {
+  const th = 4;
+  const tv = 3;
+
+  const total = th * tv;
+  const tile = Math.ceil((time / 280) % total);
+  const x = Math.ceil(tile / th);
+  const y = tile % tv || tv;
+
+  texture.repeat.set(1 / th, 1 / tv);
+  texture.offset.x = (1 / th) * x;
+  texture.offset.y = (1 / tv) * y;
+};
+
+const quaternion = new Quaternion();
+const yAxis = new Vector3(0, 1, 0);
+
+export const createPlayer = () => {
+  const texture = new TextureLoader().load('/public/Idle-copy.png');
+
+  texture.anisotropy = 4;
+  texture.wrapS = RepeatWrapping;
+  texture.wrapT = RepeatWrapping;
+  texture.center.x -= 0.01;
+  texture.center.y -= 0.02;
+
+  const material = new MeshBasicMaterial({
+    side: FrontSide,
+    map: texture,
+    color: 0xe9724f,
+    transparent: true,
+    precision: 'highp',
+  });
+  const group = new Group();
+  const geometry = new PlaneGeometry(1.8, 1.8);
+
+  const mesh = new Mesh(geometry, material);
+  const arrowHelper = createArrowHelper();
+
+  group.add(mesh, arrowHelper.mesh);
+
+  return {
+    group,
+    render(direction: Vector3, time: number) {
+      if (direction.length()) {
+        group.position.add(direction);
+        arrowHelper.render(direction);
+        quaternion.setFromUnitVectors(yAxis, direction.clone().normalize());
+      }
+      mesh.quaternion.slerp(quaternion, 0.1);
+
+      idle(texture, time);
+    },
+  };
+};
---
+++ src/screens/game/game.ts
@@ -0,0 +1,35 @@
+import { keyboard } from '../../controls/keyboard';
+import { createPlayer } from '../../objects/player';
+import { createScene } from '../../objects/scene';
+import { createCamera } from '../../objects/camera';
+import { createRenderer } from './renderer';
+
+export const GameScreen = () => {
+  const app = document.getElementById('app')!;
+  const renderer = createRenderer();
+
+  app.append(renderer.domElement);
+
+  const scene = createScene();
+  const player = createPlayer();
+  const camera = createCamera();
+
+  scene.add(player.group);
+
+  const animate = (time: number) => {
+    const direction = keyboard.directionVector;
+
+    player.render(direction, time);
+    camera.position.add(direction);
+
+    renderer.render(scene, camera);
+  };
+
+  renderer.setAnimationLoop(animate);
+
+  window.onresize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+};
